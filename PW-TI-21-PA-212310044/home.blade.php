94db68e8670ac9142d8c150bc37948dc59f5cc4b
hana yulia rahmah <80799927+hanadevelop@users.noreply.github.com>
2023-07-04 10:30:10 +0700

    Add files via upload

--- PW-TI-21-PA-212310044/home.blade.php
+++ PW-TI-21-PA-212310044/home.blade.php
@@ -1,47 +1,24 @@
-
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <div class="d-flex align-items-center p-3 my-3 text-white bg-purple
-rounded shadow-sm">
-    <!-- <img class="me-3" src="{{ url('./assets/media/icons/iconibik.png') }}" alt="icon-ibik" width="45" height="45"> -->
-    <div class="lh-1">
-        <h1 class="h6 mb-0 text-white lh-1">Bootstrap</h1>
-        <small>Since 2011</small>
-    </div>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  </head>
+  <body>
+    @include('component.navbar');
+    <div>
+    <div class="card" style="width: 18rem;">
+  <img src="\img\rusa.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">konservasi rusa</h5>
+    <p class="card-text">Rusa, sambar, atau menjangan (bahasa Inggris: deer) adalah hewan mamalia pemamah biak (ruminan) yang termasuk famili Cervidae. Salah satu ciri khas rusa adalah adanya antler (rangga), dan bukan tanduk, yang merupakan pertumbuhan tulang yang berkembang setiap tahun (biasanya pada musim panas) terutama pada rusa jantan (walaupun ada beberapa pengecualian). Ada sekitar 34 spesies rusa di seluruh dunia yang terbagi menjadi dua kelompok besar: kelompok rusa dunia lama yang termasuk subfamilia Muntiacinae dan Cervinae; serta kelompok rusa dunia baru, Hydropotinae dan Odocoilinae..</p>
+
+  </div>
 </div>
-<div class="card">
-    <table class="table">
-        <thead>
-            <tr>
-                <th>No</th>
-                <th>NPM</th>
-                <th>Nama</th>
-                <th>Gender</th>
-                <th>Alamat</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($data['students'] as $index => $result)
-            <tr>
-                <td>{{ $index+1 }}</td>
-                <td>{{ $result['npm'] }}</td>
-                <td>{{ $result['name'] }}</td>
-                <td>{{ $result['gender'] }}</td>
-                <td>{{ $result['alamat'] }}</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-    <div class="card-body">
-        Welcome folks..
     </div>
-</div>
-</body>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+  </body>
 </html>
